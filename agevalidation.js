1f5e50244275c06ac034b38143d6c0faf097eb8d
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-06-16 22:06:47 +0530

    agecalculator

---
+++ agevalidation.js
@@ -0,0 +1,8 @@
+let age = prompt("enter age");
+if(age>=18){
+    console.log("eligible for vote");
+
+}
+else{
+    console.log("not eligible")
+}
---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+alert("Welcome To js");
+let name = "Rahul";
+let age = 20;
+
+console.log(name);
+console.log(age);
+let student = prompt("Enter your name");
+alert("Welcome " + student);
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+
+console.log("Sum = " + sum);
+let studentName = prompt("Enter your name");
+let department = prompt("Enter your department");
+
+alert("Welcome " + studentName);
+
+console.log("Name: " + studentName);
+console.log("Department: " + department);
+
